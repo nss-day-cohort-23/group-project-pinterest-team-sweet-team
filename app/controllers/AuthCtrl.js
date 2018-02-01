5b2c2a1491b549a58548aa538f7800fa992b9406
Sarah Morris <sarah.morris9@gmail.com>
2018-02-01 15:50:48 -0600

    Added boilerplate code for controllers and index.html comments

---
+++ app/controllers/AuthCtrl.js
@@ -0,0 +1,5 @@
+'use strict';
+
+angular.module("SweetApp").controller("AuthCtrl", function($scope) {
+
+});
---
+++ app/controllers/BoardCtrl.js
@@ -0,0 +1,5 @@
+'use strict';
+
+angular.module("SweetApp").controller("BoardCtrl", function($scope) {
+
+});
