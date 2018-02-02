0cf94fba873798991c6b8db67ab0a341a0060458
Spencer York <spenceryork@Bernies-MacBook-Pro.local>
2018-02-02 10:23:46 -0600

    initial controller functions created

--- app/controllers/PinCtrl.js
+++ app/controllers/PinCtrl.js
@@ -1,5 +1,21 @@
 'use strict';
 
-angular.module("SweetApp").controller("PinCtrl", function($scope) {
+angular.module("SweetApp").controller("PinCtrl", function($scope, FbFactory, $routeParams, $location) {
+
+    $scope.newPin = {
+        uid: "",
+        name: "",
+        url: ""
+    };
+
+    $scope.savePin = () => {
+        console.log("New Pin", $scope.newPin);
+        // I think "$routeParams.id" will be the boards id?
+        $scope.newPin.boardId = $routeParams.id;
+        FbFactory.addPin($scope.newPin)
+        .then( (data) => {
+            $location.url(`/boards/${$routeParams.id}`);
+        });
+    };
 
 });
